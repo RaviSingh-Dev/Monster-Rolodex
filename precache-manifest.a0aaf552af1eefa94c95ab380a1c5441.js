f7ed7cceefd0e53e8fbef2b3fe3215284748211a
Ravi kumar singh <rk758290@gmail.com>
2020-09-17 12:56:32 +0200

    Updates

---
+++ precache-manifest.a0aaf552af1eefa94c95ab380a1c5441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6113574a478722936df182f17399be",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e31ed939cae612bf0904",
+    "url": "/monster-rolodex/static/css/main.830f98a7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31ed939cae612bf0904",
+    "url": "/monster-rolodex/static/js/main.01805a41.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
